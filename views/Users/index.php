52a9c435ec4914b748b397d87325c6aae590dfcb
elvie.estoesta <elvira.eatoesta@robertsoncollege.net>
2014-12-17 16:40:48 -0600

    modified index.php

---
+++ views/Users/index.php
@@ -0,0 +1,23 @@
+<table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      
+      <th>UserID</th>
+      <th>FirstName</th>
+      <th>LastName</th>
+      <th>Email</th>
+      <th>Password</th>
+      <th>SecurityLevel</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+        <td>example</td>
+       <td>example</td>
+       <td>example</td>
+        <td>example</td>
+         <td>example</td>
+          <td>example</td>
+    </tr>
+  </tbody>
+</table>
