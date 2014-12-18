a0520a2a5f394a7e2e1513b77fa7a6ebb5c73e83
elvie.estoesta <elvira.eatoesta@robertsoncollege.net>
2014-12-18 14:07:16 -0600

    modified index.php in task

---
+++ views/task/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>TaskID</th>
+            <th>TaskName</th>
+            <th>ChecklistID</th>
+            <th>TaskTime</th>           
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="info">
+            <td>example</td>
+            <td>example</td>
+            <td>example</td>
+            <td>example</td>
+        </tr>
+    </tbody>
+</table>
