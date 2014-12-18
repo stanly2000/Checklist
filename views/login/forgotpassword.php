b1f23e1ee7a1f330ac87ae210f38a1aea418d469
elvie.estoesta <elvira.eatoesta@robertsoncollege.net>
2014-12-18 11:30:52 -0600

    modified forgotpassword.php

--- views/login/forgotpassword.php
+++ views/login/forgotpassword.php
@@ -2,14 +2,12 @@
 
 ?>
 <h1>Forgot Password</h1>
-<h2>Enter you email>
+<h2>Enter you email</h2>
     
-    <div class="form group">
-        <label class="col-md-2 control-label" for="UserName">Email</label>
+    <div class="form group">    
         <div class="col-md-10"></div>
         <input class="form-control" data-val="true" data-val-required="The email field is required" id="Email" name="Email"
-               type="text" value=""/>
-        
+               type="text" value=""/>   
    <div class="col-md-10">
         </div>
         </div>
