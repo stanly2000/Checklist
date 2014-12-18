5c32ddc8434de206f7e403abe3cfe5f591856c64
stanly2000 <shlomo.margulets@robertsoncollege.net>
2014-12-18 15:34:27 -0600

    All Checklist function (exept get one) work with stored procedures now

--- models/Checklist.php
+++ models/Checklist.php
@@ -17,25 +17,33 @@ class Checklist {
     }
     public function getAll( )
     {       
-        $stmt = $this->db->prepare("SELECT * FROM tbChecklist");
+        $stmt = $this->db->prepare("CALL spGetChecklists");
         $stmt->execute();
         return   $stmt->fetchAll(PDO::FETCH_OBJ);
     }
     public function update($params = [])
     {
-$stmt = $this->db->prepare("UPDATE tbChecklist SET ChecklistName=? WHERE ChecklistID=?");
-$stmt->execute(array($params['title'], $params['id']));
-//$affected_rows = $stmt->rowCount();
+
+        try{
+    $stmt = $this->db->prepare("CALL spUpdateChecklist (?,? ) ");
+    $stmt->execute(array( $params['id'],$params['title']));
+        }
+        catch(PDOException $ex) {
+    echo "An Error occured!"; //user friendly message
+    echo $ex->getMessage();
+    }
+
     }
     public function add($params = [])
     {
-$stmt = $this->db->prepare("INSERT INTO  tbChecklist (ChecklistName) VALUES (?) ");
-$stmt->execute(array($params['title']));
-//$affected_rows = $stmt->rowCount();
+    $stmt = $this->db->prepare("CALL spInsertChecklist (:p_ChecklistName) ");
+    $stmt->bindValue(':p_ChecklistName', $params['title'], PDO::PARAM_STR);
+    $stmt->execute();
     }
     public function remove($params = [])
     {
-$stmt = $this->db->prepare("DELETE FROM tbChecklist  WHERE ChecklistID=?");
-$stmt->execute(array( $params['id']));
+    $stmt = $this->db->prepare("CALL spDeleteChecklist (:p_ChecklistID)");
+    $stmt->bindValue(':p_ChecklistID', $params['id'], PDO::PARAM_INT);
+    $stmt->execute();
     }
 }
