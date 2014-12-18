61eefa1c96cad4f89613433f9cdc059cefbf3353
elvie.estoesta <elvira.eatoesta@robertsoncollege.net>
2014-12-18 10:58:46 -0600

    modified forgotpassword.php

--- views/login/forgotpassword.php
+++ views/login/forgotpassword.php
@@ -1,13 +1,18 @@
 <?php
 
 ?>
+<h1>Forgot Password</h1>
+
+<h2>Enter you email>
+    <div class="form group">
+        <label class="col-md-2 control-label" for="UserName">Email</label>
 <div class="form group">
    <div class="col-md-10">
         </div>
         </div>
 <div class="form-group">
     <div class="col-mid-offswt-2 col-md-10">
-        <input type="submit" class="btn btn-default" value="forgot password"/>
+        <input type="submit" class="btn btn-default" value="submit" id="linkbtn"/>
     </di>
     </div>
         
