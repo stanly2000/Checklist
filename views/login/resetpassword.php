e07c5abbeedb1b107336a4e0b5a2eaa40b65ea7d
elvie.estoesta <elvira.eatoesta@robertsoncollege.net>
2014-12-18 15:42:08 -0600

    modified reserpassword.php in login

---
+++ views/login/resetpassword.php
@@ -0,0 +1,37 @@
+
+<h1>Reset Password</h1>
+
+         <div class="form group">    
+        <div class="col-md-10"></div>
+        <label class="col-md-2 control-label" for="New Password">New Password:</label>
+        <input class="form-control" data-val="true" data-val-required="The email field is required" id="NewPassword" name="New Password"
+               type="text" value=""/>   
+        </div>
+        </div>
+        &nbsp;
+        &nbsp;
+         &nbsp;
+        <div class="form group">    
+        <div class="col-md-10"></div>
+         <label class="col-md-2 control-label" for="Retype Password">Retype Password:</label>
+        <input class="form-control" data-val="true" data-val-required="The email field is required" id="RetypePassword" name="Retype Password"
+               type="text" value=""/>  
+        </div>
+        </div>  
+               &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+        <div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <input type="submit" value="submit" class="btn btn-default"/>
+               
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                
+                <input type="submit" value="cancel" class="btn btn-default"/>
+       
+        </div>
+        </div>
+         
