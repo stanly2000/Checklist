cda3205aa86a6cbbf71b152ef056f8c0b63bb0c0
stanly2000 <shlomo.margulets@robertsoncollege.net>
2014-12-18 13:35:44 -0600

    completed crud on checklists

--- views/checklist/index.php
+++ views/checklist/index.php
@@ -9,7 +9,7 @@
             <thead>
                 <tr>
                     <th>Name</th>
-                    <th>Actions &nbsp;&nbsp;&nbsp;<a href="<?php echo RESOURCE; ?>/checklist/add">Add New </a></th>
+                    <th>Actions &nbsp;&nbsp;&nbsp;<a class="font20" href="<?php echo RESOURCE; ?>/checklist/add">Add New </a></th>
                 </tr>
             </thead>
             <tbody>
@@ -18,17 +18,29 @@
            <td><?php echo $clist->ChecklistName; ?></td>
            <td><a href="<?php echo RESOURCE; ?>/checklist/view/<?php echo $clist->ChecklistID ?>" >view</a>&nbsp;
            <a href="<?php echo RESOURCE; ?>/checklist/update/<?php echo $clist->ChecklistID ?>" >update</a>&nbsp;
-           <a href="#" class='delLink' >delete</a></td>
+           
+           <a id="dlink_<?php echo $clist->ChecklistID ?>" href="#" class='delLink' >delete</a>
+
        </tr>
      <?php } ?>
             </tbody>
         </table>
     </div>
       <div class="col-lg-3">&nbsp;</div>
-      <form method="POST" action="<?php echo RESOURCE ;?>/home/">
-          <input type="hidden" name="id" value="">
+      <form id="rmform" method="POST" action="<?php echo RESOURCE ;?>/home/">
+          <input type="hidden" name="id" id="id" value="">
           <input type="hidden" name="cntr" value="<?php echo $activeController;?>" >
           <input type="hidden" name="actn" value="rmPost" >
       </form>
 </div>
 
+<script>
+    $(document).ready(function(){
+        $('.delLink').on('click',function(){
+             tmp = $(this).attr('id').split('_');
+             idToDel = tmp[1];
+             $('#id').val(idToDel);
+             $('#rmform').submit();
+        });
+    });
+    </script>
