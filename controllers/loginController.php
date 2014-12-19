9a5391de0f4bb564bf9ad5738ddc2b8853b55b77
erikajoycruz <erika_joy.cruz@robertsoncollege.net>
2014-12-19 14:50:24 -0600

    Changed the link to be redirected

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -12,17 +12,20 @@ class LoginController extends Controller {
             $stmt = $model->loginCheck($_POST['Email'], $_POST['Password']);
             if ($stmt != null) {
                 //TODO create session
-                //CRESTE INSTANCE LoggedUser Class
+                //CREATE INSTANCE LoggedUser Class
                 // PUT this class to session 
                 foreach($stmt as $rows)
                 {
                     $_SESSION['UserID'] = $rows['UserID'];
                     $_SESSION['SecurityLevel'] = $rows['SecurityLevel'];
+                    echo 'im here';
                 }
-                $this->redirect('home');
+//                $this->redirect('home');
             }
             else{
-                $this->redirect('login/index');
+//                $this->redirect('login/index');
+                echo 'im not here';
             }
+            die();
     }
 }
