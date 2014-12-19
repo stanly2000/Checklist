134f9779b0bdd536db1530f08799760b621437d8
erikajoycruz <erika_joy.cruz@robertsoncollege.net>
2014-12-19 11:14:33 -0600

    Added Crud for Task.php (model)

--- models/Task.php
+++ models/Task.php
@@ -3,9 +3,9 @@
 class Task { 
      public $db;
      public $TaskID;
-     public $Name;
+     public $TaskName;
      public $ChecklistID;
-     public $Time;
+     public $TaskTime;
      
       function __construct($db) {
         $this->db = $db;
@@ -18,8 +18,32 @@ class Task {
     }
     public function getAll( )
     {       
-        $stmt = $this->db->prepare("SELECT * FROM tbTask");
+        $stmt = $this->db->prepare("CALL spGetTask");
         $stmt->execute();
         return   $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+        public function update($params = [])
+    {
+        try{
+               $stmt = $this->db->prepare("CALL spUpdateTask (?,? ) ");
+               $stmt->execute(array( $params['id'],$params['title']));
+        }
+        catch(PDOException $ex) {
+                echo "An Error occured!"; 
+                echo $ex->getMessage();
+    }
+
+    }
+    public function add($params = [])
+    {
+         $stmt = $this->db->prepare("CALL spInsertTask (:p_TaskName) ");
+         $stmt->bindValue(':p_TaskName', $params['title'], PDO::PARAM_STR);
+         $stmt->execute();
+    }
+    public function remove($params = [])
+    {
+         $stmt = $this->db->prepare("CALL spDeleteTask (:p_TaskID)");
+         $stmt->bindValue(':p_TaskID', $params['id'], PDO::PARAM_INT);
+         $stmt->execute();
     }
 }
