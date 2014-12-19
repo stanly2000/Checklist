246fe121c1a39ec0c6fca00cb9daeed3c2a77db6
erikajoycruz <erika_joy.cruz@robertsoncollege.net>
2014-12-19 09:33:33 -0600

    Added functions in Task.php

--- models/Task.php
+++ models/Task.php
@@ -10,3 +10,18 @@ class Task {
       function __construct($db) {
         $this->db = $db;
     }
+        public function get( $id)
+    {       
+        $stmt = $this->db->prepare("SELECT * FROM tbTask WHERE TaskID=?");
+        $stmt->execute(array($id));
+        return   $stmt->fetch(PDO::FETCH_OBJ);
+    }
+    public function getAll( )
+    {       
+        $stmt = $this->db->prepare("SELECT * FROM tbTask");
+        $stmt->execute();
+        return   $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+}
+
+    
