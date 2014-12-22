e9cf38a8d10e26b3189e13cbe3742371b861b42a
elvie.estoesta <elvira.eatoesta@robertsoncollege.net>
2014-12-22 11:16:26 -0600

    start doing the userController.php

--- controllers/userController.php
+++ controllers/userController.php
@@ -1,8 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class userController extends Controller
+{
+    
+    public function index()
+    {
+        $model=$this->model('User');
+        $user = $model->getAll();
+        $this->render( __CLASS__,__FUNCTION__,'user/index',['user'=>$user ]);
+ 
+    }
+    
+
+}
 
