2b064628a7ac488b3d82b367a045121f97f47122
stanm97 <stanly_97@yahoo.com>
2014-12-26 15:19:10 -0600

    small fix in redirect (removed two slashes in url)

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,4 +1,5 @@
 <?php
+require_once APP . '/utilities/DebugLogger.php';
 
 class Controller
 {
@@ -7,44 +8,52 @@ class Controller
 
     public $model = null;
 
-
     function __construct()
     {
-        $this->db = $this->db_connect();//new PDO('mysql:host=localhost;dbname=Checklist;charset=utf8', 'root', '');
-        
-    }
-    
-    private function db_connect(){
-          return  new PDO('mysql:host=localhost;dbname=Checklist;charset=utf8', 'root', '');
+        $this->db = $this->db_connect(); // new PDO('mysql:host=localhost;dbname=Checklist;charset=utf8', 'root', '');
     }
 
+    private function db_connect()
+    {
+        return new PDO('mysql:host=localhost;dbname=Checklist;charset=utf8', 'root', '');
+    }
 
     public function model($model)
     {
-       require APP . '/models/' .$model. '.php';
+        require APP . '/models/' . $model . '.php';
         
-       return new $model($this->db);
+        return new $model($this->db);
     }
-    
-    public function render($activeController, $activeControllerMethod, $view, $data = [],$callbackMethod = null){
-        
+
+    public function render($activeController, $activeControllerMethod, $view, $data = [], $callbackMethod = null)
+    {
         $activeController = strtolower(str_replace('Controller', '', $activeController));
         require APP . 'views/_shared/header.php';
-        require APP . 'views/'.$view.'.php';
+        require APP . 'views/' . $view . '.php';
         require APP . 'views/_shared/footer.php';
     }
-    
-    public function redirect($Controller, $ControllerMethod = null, $params = []){
+
+    public function redirect($Controller, $ControllerMethod = null, $params = [])
+    {
+        echo "($Controller, $ControllerMethod ,  ";
         $Controller = strtolower(str_replace('Controller', '', $Controller));
-        header('Location: '.RESOURCE.'/'.$Controller.'/'.$ControllerMethod.'/'.$this->builParams($params));
-        exit;
+        $url = RESOURCE . '/' . $Controller . '/';
+        if ($ControllerMethod != '') {
+            $url .= $ControllerMethod . '/';
+        }
+        if (count($params) > 0) {
+            $url .= $this->builParams($params);
+        }
+        header('Location:' . $url);
+        exit();
     }
-    
-    private function builParams($params = []){
+
+    private function builParams($params = [])
+    {
         $param_str = "";
-        if (count($params) > 0){
+        if (count($params) > 0) {
             foreach ($params as $val) {
-                $param_str .= $val.'/';
+                $param_str .= $val . '/';
             }
         }
         return trim($param_str);
