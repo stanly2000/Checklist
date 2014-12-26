0647490a86b1049ffffb689c481ceaa13bafac87
stanm97 <stanly_97@yahoo.com>
2014-12-26 15:22:49 -0600

    just format php files

--- app/models/Checklist.php
+++ app/models/Checklist.php
@@ -1,85 +1,109 @@
 <?php
 require_once 'IDbModels.php';
-require_once APP.'/utilities/DebugLogger.php';
-class Checklist implements IDbModels{
+require_once APP . '/utilities/DebugLogger.php';
+
+class Checklist implements IDbModels
+{
+
     private $db;
+
     public $ChecklistID;
+
     public $ChecklistName;
+
     public $Tasks = [];
-    public $validationRules = []; 
-    
-    function __construct($db) {
+
+    public $validationRules = [];
+
+    function __construct($db)
+    {
         $this->db = $db;
-        $this->validationRules = ['title'=>['notEmpty','lettersAndNumbers'],
-            'id'=>['notEmpty','isInteger']];
+        $this->validationRules = [
+            'title' => [
+                'notEmpty',
+                'lettersAndNumbers'
+            ],
+            'id' => [
+                'notEmpty',
+                'isInteger'
+            ]
+        ];
     }
-    
-    public function load($id){
+
+    public function load($id)
+    {
         $stmt = $this->db->prepare("SELECT * FROM tbChecklist WHERE ChecklistID=?");
-        $stmt->execute(array($id));
+        $stmt->execute(array(
+            $id
+        ));
         $res = $stmt->fetch(PDO::FETCH_OBJ);
         $this->ChecklistID = $res->ChecklistID;
         $this->ChecklistName = $res->ChecklistName;
         if ($this->ChecklistID == null)
             return FALSE;
-        else 
+        else
             return true;
     }
-    
-    public function getTasks(){
-        try{
 
-        $stmt = $this->db->prepare("SELECT t.TaskID, TaskName, ChecklistID, PropertyName, PropertyAttribute, PropertyValue  FROM `tbTask` as t  LEFT JOIN `tbTaskProperties` as p  ON (t.TaskID = p.TaskID) WHERE t.ChecklistID = ?");
-        $stmt->execute(array($this->ChecklistID));
-        $this->Tasks = $stmt->fetchAll(PDO::FETCH_OBJ);
-        return true;
-        }
-        catch (PDOException $ex){ 
+    public function getTasks()
+    {
+        try {
+            
+            $stmt = $this->db->prepare("SELECT t.TaskID, TaskName, ChecklistID, PropertyName, PropertyAttribute, PropertyValue  FROM `tbTask` as t  LEFT JOIN `tbTaskProperties` as p  ON (t.TaskID = p.TaskID) WHERE t.ChecklistID = ?");
+            $stmt->execute(array(
+                $this->ChecklistID
+            ));
+            $this->Tasks = $stmt->fetchAll(PDO::FETCH_OBJ);
+            return true;
+        } catch (PDOException $ex) {
             return false;
         }
     }
-    
-    public function get( $id)
-    {       
+
+    public function get($id)
+    {
         $stmt = $this->db->prepare("SELECT * FROM tbChecklist WHERE ChecklistID=?");
-        $stmt->execute(array($id));
-        return   $stmt->fetch(PDO::FETCH_OBJ);
+        $stmt->execute(array(
+            $id
+        ));
+        return $stmt->fetch(PDO::FETCH_OBJ);
     }
-    public function getAll( )
-    {       
+
+    public function getAll()
+    {
         $stmt = $this->db->prepare("CALL spGetChecklists");
         $stmt->execute();
-        return   $stmt->fetchAll(PDO::FETCH_OBJ);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
-    public function remove($id)//$params = [])
+    public function remove($id) // $params = [])
     {
-        try{
-    $stmt = $this->db->prepare("CALL spDeleteChecklist (:p_ChecklistID)");
-    $stmt->bindValue(':p_ChecklistID', $id, PDO::PARAM_INT);
-    $stmt->execute();
-    return true;
-        }
-        catch (PDOException $ex){ 
+        try {
+            $stmt = $this->db->prepare("CALL spDeleteChecklist (:p_ChecklistID)");
+            $stmt->bindValue(':p_ChecklistID', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $ex) {
             return false;
         }
     }
 
-    public function save(){
-        
-        try{
-            if ($this->ChecklistID){
-        $stmt = $this->db->prepare("CALL spUpdateChecklist (?,? ) ");
-        $stmt->execute(array( $this->ChecklistID,$this->ChecklistName));
+    public function save()
+    {
+        try {
+            if ($this->ChecklistID) {
+                $stmt = $this->db->prepare("CALL spUpdateChecklist (?,? ) ");
+                $stmt->execute(array(
+                    $this->ChecklistID,
+                    $this->ChecklistName
+                ));
+            } else {
+                $stmt = $this->db->prepare("CALL spInsertChecklist (:p_ChecklistName) ");
+                $stmt->bindValue(':p_ChecklistName', $this->ChecklistName, PDO::PARAM_STR);
+                $stmt->execute();
             }
-            else {
-                 $stmt = $this->db->prepare("CALL spInsertChecklist (:p_ChecklistName) ");
-            $stmt->bindValue(':p_ChecklistName', $this->ChecklistName, PDO::PARAM_STR);
-            $stmt->execute();
-            }
-        return true;
-        }
-        catch (PDOException $ex){ 
+            return true;
+        } catch (PDOException $ex) {
             return false;
         }
     }
