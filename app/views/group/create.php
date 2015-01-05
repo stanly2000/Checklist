6abce79cb77e2bfa365e7a7cd652c759eb4a1604
erikajoycruz <erika_joy.cruz@robertsoncollege.net>
2015-01-05 15:17:25 -0600

    Modified create.php for groups

--- app/views/group/create.php
+++ app/views/group/create.php
@@ -3,10 +3,16 @@
         <title>Create Group</title>
     </head>
     <body>
-        <form method="post" action="<?php echo RESOURCE ;?>/group/CreateGroup">
-            Name: <input type="text" name="groupname" id="groupname" />
-            <input type="submit" value="Back" onclick="document.location.href='http://localhost/checklist/public/group'" />
-            <input type="submit" value="Add" />
+        <form method="post" action="<?php echo RESOURCE ;?>/group/CreateGroup" class="form-horizontal">
+            <div class="form-group">
+            <label for="Name" class="col-lg-2 control-label"> Group Name: </label>
+             <div class="col-lg-3">
+            <input type="text" class="form-control" input type="text" name="groupname" id="groupname">
+             </div>
+            </div>
+            
+            <input type="submit" value="Back" onclick="document.location.href='http://localhost/checklist/public/group'" class="btn btn-success" />
+            <input type="submit" value="Add New Group" class="btn btn-success"/>
         </form>
     </body>
 </html>
