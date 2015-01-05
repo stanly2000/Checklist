ba95141520a2aad8ce3fd23f8d78cea461f4bff8
erikajoycruz <erika_joy.cruz@robertsoncollege.net>
2015-01-05 10:35:46 -0600

    Modified update.php in Groups

--- app/views/group/update.php
+++ app/views/group/update.php
@@ -3,22 +3,20 @@
         <title>Group Update</title>
     </head>
     <body>
-        <form method="post" action="<?php echo RESOURCE ;?>/group/UpdateGroup">
+        <form method="post" action="<?php echo RESOURCE ;?>/group/UpdateGroup" class="form-horizontal">
           <table>
             <?php foreach($data['update'] as $g) { ?>
               <input type="hidden" value="<?php echo $g->GroupID ?>" id="GroupID" name="GroupID"/>
-            <tr>
-                <td>
-                    Group name:&nbsp;
-                </td>
-                <td>
-                    <input type="text" value="<?php echo $g->GroupName ?>" id="UGName" name="UGName"/>
-                </td>
-            </tr>
+            <div class="form-group">
+            <label for="GroupName" class="col-lg-2 control-label">Group Name</label>
+             <div class="col-lg-3">
+            <input type="text" class="form-control" type="text" value="<?php echo $g->GroupName ?>" id="UGName" name="UGName">
+             </div>
+            </div>
             <?php } ?>
         </table>
-            <input type="submit" name="UGbtn" value="Back" onclick="document.location.href = 'http://localhost/checklist/public/group'"/>
-            <input type="submit" value="Update" />
+            <input type="submit" name="UGbtn" value="Back" onclick="document.location.href = 'http://localhost/checklist/public/group'" class="btn btn-success"/>
+            <input type="submit" value="Update" class="btn btn-success" />
         </form>
     </body>
 </html>
